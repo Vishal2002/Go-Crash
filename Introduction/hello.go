f473450ce975e473b8c88318e8240920a778f861
Vishal <vishalsharma05052002@gmail.com>
2024-07-28 19:25:18 +0530

    First Go Program

---
+++ Introduction/hello.go
@@ -0,0 +1,8 @@
+package main //main is a special package name that tells Go this is an executable program, not a library.It can only file that can be executed
+import "fmt" //It is a package and we use this package for formatting and printing purpose
+
+func main() {
+
+	fmt.Println("Hello world!")
+
+}
